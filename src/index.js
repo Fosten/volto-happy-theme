91981d3eba7c29add4fc2a0898c0cc68115dceb9
fosten <fosten@gmail.com>
2023-02-11 23:29:42 -0500

    Import initialBlocks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // All your imports required for the config here BEFORE this line
 import '@plone/volto/config';
+import { initialBlocks } from '@Fosten/volto-happy-theme/config/Blocks'
 
 export default function applyConfig(config) {
   // Add here your project's configuration here by modifying `config` accordingly
   config.blocks = {
     ...config.blocks,
     blocksConfig: {
-      ...config.blocks.blocksConfig},
+      ...config.blocks.blocksConfig,
+      ...initialBlocks},
     }
   return config;
 }
