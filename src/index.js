d3a8711ef3db8188e35ca33e84348a83a761ad7d
Fosten <fosten@gmail.com>
2024-08-21 15:35:05 -0400

    Use navroot in page title

--- src/index.js
+++ src/index.js
@@ -15,5 +15,12 @@ export default function applyConfig(config) {
       ...customBlocks,
     },
   };
+
+  //include site title in <title>
+  if (!config.settings.siteTitleFormat) {
+    config.settings.siteTitleFormat = { includeSiteTitle: true };
+  } else config.settings.siteTitleFormat.includeSiteTitle = true;
+  config.settings.titleAndSiteTitleSeparator = '|';
+
   return config;
 }
