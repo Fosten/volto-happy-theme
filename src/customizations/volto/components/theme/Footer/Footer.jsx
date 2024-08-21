6179531a2b01762f7ec20b5f1ca4e831835985c9
Fosten <fosten@gmail.com>
2024-08-21 15:35:26 -0400

    Add sitemap to footerbar

--- src/customizations/volto/components/theme/Footer/Footer.jsx
+++ src/customizations/volto/components/theme/Footer/Footer.jsx
@@ -86,15 +86,16 @@ const Footer = ({ intl }) => (
             <div className="site-email">
               <a href="mailto:info@happybaseball.com">info@happybaseball.com</a>
             </div>
-            <div className="youtube"></div>
-            <div className="twitter">
+            <div className="powered-by-plone">
               <a href="https://plone.org" target="_blank" rel="noreferrer">
                 Powered by Plone
               </a>
             </div>
-            <div className="facebook"></div>
             <div className="accessibility">
               <a href="/accessibility">Accessibility</a>
+            </div>
+            <div className="sitemap">
+              <a href="/sitemap">Sitemap</a>
             </div>
           </div>
         </div>
