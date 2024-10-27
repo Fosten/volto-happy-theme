cd4eea65d5159d717139416a64eb7030d94e29d9
Fosten <fosten@gmail.com>
2024-10-27 12:14:39 -0400

    Remove Byline block

--- packages/volto-happy-theme/src/index.js
+++ packages/volto-happy-theme/src/index.js
@@ -1,20 +1,7 @@
 // All your imports required for the config here BEFORE this line
-import '@plone/volto/config';
-import customBlocks from './components/Blocks/customBlocks';
 
 export default function applyConfig(config) {
   // Add here your project's configuration here by modifying `config` accordingly
-  config.blocks = {
-    ...config.blocks,
-    initialBlocks: {
-      'News Item': ['title', 'description', 'byline', 'leadimage', 'slate'],
-    },
-    blocksConfig: {
-      ...config.blocks.blocksConfig,
-      ...config.blocks.initialBlocks,
-      ...customBlocks,
-    },
-  };
 
   //include site title in <title>
   if (!config.settings.siteTitleFormat) {
